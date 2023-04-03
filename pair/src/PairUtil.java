3d6781721173779e33b1c82a73c9b0e1a5c274d3
Настя <nastiy0205@gmail.com>
2023-04-03 17:44:10 +0300

    Commit

--- pair/src/PairUtil.java
+++ pair/src/PairUtil.java
@@ -3,9 +3,8 @@ public class PairUtil {
     public boolean Equals(Pair pair1,Pair pair2){
         if(pair1.getFirst().equals(pair2.getFirst())&&pair1.getSecond().equals(pair2.getSecond())){
             return true;
-        } else {
-            return false;
         }
+        return false;
     }
 
     public Pair Copy(Pair pair1){
@@ -26,16 +25,15 @@ public class PairUtil {
         pair1.setSecond(pair2.getFirst());
     }
     public static void main(String[] args) {
-        Pair pair1=new Pair("123","345");
-        Pair pair2=new Pair(123,345);
-        Pair pair3=new Pair("123",345);
-        Pair pair4=new Pair("123","345");
+        Pair pair1=new Pair<String>("123","345");
+        Pair pair2=new Pair<Integer>(123,345);
+        Pair pair4=new Pair<String>("123","345");
 
         PairUtil pairUtil=new PairUtil();
 
         System.out.println("function Equals for pair1(\"123\",\"345\") and pair4(\"123\",\"345\"): "+pairUtil.Equals(pair1,pair4));
         System.out.println("function Copy for pair2(123,345): "+pairUtil.Copy(pair2).getFirst()+","+pairUtil.Copy(pair2).getSecond());
-        pairUtil.Swap(pair3);
-        System.out.println("function Swap for pair3(\"123\",345): "+pair3.getFirst()+","+pair3.getSecond());
+        pairUtil.Swap(pair4);
+        System.out.println("function Swap for pair4(\"123\",\"345\"): "+pair4.getFirst()+","+pair4.getSecond());
     }
 }
